f84d08a12d39eec81731ee455aa7f5c936abe308
AVAZBEK-QAXXOROV <avazbekqaxxorov461@gmail.com>
2024-04-25 11:46:14 +0500

    java script 2 homework

---
+++ page/1-masala/script/app.js
@@ -0,0 +1,7 @@
+let info = prompt("Assalomu aleykum foydalanuvchi to'rtburchakning perimetrini aniqlaymizmi: ");
+
+const height = Number(prompt("To'rtburchakning balanligini kiriting: "));
+
+const width = Number(prompt("To'rtburchakning uzunligini kiriting: "));
+let yigindi = height + width 
+console.log(yigindi * 2 )
---
+++ page/2-masala/script/app.js
@@ -0,0 +1,31 @@
+let from = prompt("Qayerda tavallud topgansiz: ");
+
+let move = prompt("Kelajakda ham shu davlatda qolmoqchimisiz")
+
+let region = prompt("Qaysi viloyatda:  ");
+
+let age = prompt("Yoshingiz nechida: ");
+
+let family = prompt("Oilada nechita farzandsizlar: ");
+
+let xoby = prompt("Nimalarga qiziqasiz: ");
+
+let occupation = prompt("Kelajakda qaysi kasb egasi bo'lmoqchisiz: ");
+
+let like = prompt("Qanaqa mashinalarga qiziqasiz: ");
+
+let dislike = prompt("Sizga nimalar yoqmaydi: ");
+
+let life = prompt("Oxirgi savol Siz hayotingizdan mamnunmisiz: ");
+
+document.write(
+    "Siz:" + from + " " 
+/n + "Tanlovingizni o'ylab ko'rdizmi: " + move + " "
+/n + "Siz yashaydigan viloyat: " + region + " "
+/n + "Sizning yoshingiz: " + age + " " 
+/n + "Oilada nechta farzansizlar: " + family + " " 
+/n + "Sizning qiziqishlaringiz: " + xoby + " " 
+/n + "Sizning kelajakdagi Kasbingiz: " + occupation +" " 
+/n + "Siz yoqtirgan mashinangiz: " + like +" " 
+/n + "Siz yoqtirmagan narsalar: " + dislike +" " 
+/n + "Hayotingizdan mamnunmisiz" + life + " " + " Savollarga javob berganingiz uchun rahmat")
